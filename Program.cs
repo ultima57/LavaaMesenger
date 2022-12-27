e6dfac448e367234e117e6641fb066b4a7ae855d
Artem Zybkovets <artyomzar83@gmail.com>
2022-12-27 14:38:55 +0200

    added server

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace LavaaMesendjer.ConsoleClient.obj {
             Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
             Console.WriteLine(deserializedMsg.ToString());
             Console.WriteLine(msg.ToString());
+            //{ "Username":"Gosling","MessageText":"drive","TimeStamp":"2022-12-27T12:02:31.1084078Z"}
+            //Gosling < 27 / 12 / 2022 12:02:31 >: drive
         }
     }
 }
