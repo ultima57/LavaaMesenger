b746c54c21350f98bae7b2f019ef39c5589f3e92
Artem Zybkovets <artyomzar83@gmail.com>
2022-12-27 14:39:54 +0200

    removed server

---
+++ ASPCoreServer/Controllers/Messenger.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPCoreServer.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Messenger : ControllerBase {
+
+
+        // GET api/<Messenger>/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "Rayner" + id.ToString();
+        }
+
+        // POST api/<Messenger>
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+
+    }
+}
