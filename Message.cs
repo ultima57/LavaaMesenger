e7df7d33e2f2d58de8230ee3f5a83abd1add4eb0
Artem Zybkovets <artyomzar83@gmail.com>
2022-12-27 13:49:21 +0200

    added class message

---
+++ Message.cs
@@ -0,0 +1,23 @@
+﻿namespace LavaaMesendjer.ConsoleClient.obj {
+    public class Message {
+        public string Username { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Message(string username, string messageText, DateTime timeStamp) {
+            Username = username;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+
+        public Message() {
+            Username = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string? ToString() {
+            string ouput = String.Format("{0} <{2}>: {1} ", Username, MessageText, TimeStamp);
+            return ouput;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LavaaMesendjer.ConsoleClient.obj {
+    class Program {
+        static void Main(string[] args) {
+            Message msg = new Message();
+            Console.WriteLine("Begin!");
+            Console.WriteLine(msg.ToString());
+        }
+    }
+}
